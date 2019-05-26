c0c131d0d06a9beb1cc8367b874829fd8c4ddd44
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-05-26 14:49:00 +0300

    Update app.js
    add todos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,73 +1,37 @@
-let myTodoList = document.querySelector('#todo-list');
-
-myTodoList.addEventListener("click", function(event){
-
-    if(event.originalTarget.className == 'fa fa-remove'){
-        // console.log('dogru');
-        // console.log(event.target.parentElement.parentElement);
-        myTodoList.removeChild(event.target.parentElement.parentElement);
-        
-        let todos;
-
-        if(localStorage.getItem("todos") != null){
-            todos = JSON.parse(localStorage.getItem("todos"));
-
-            localStorage.clear();
-
-            let index = todos.indexOf(event.target.parentElement.parentElement.textContent);
-            if (index !== -1)
-            todos.splice(index, 1);
-            
-            if(todos.length == 0)
-            localStorage.clear();
-        
-            localStorage.setItem("todos", JSON.stringify(todos));
-        }      
-    }
-
-})
-
-
-let getTodos = function () {
-    // console.log("Gorevler cekiliyor.");
-    let todos;
-    const todoList = document.querySelector('#todo-list');
-    todos = JSON.parse(localStorage.getItem("todos"));
-
-    if(todos != null){
-        console.log(todos);
-        for (let i = 0; i < todos.length;i++){
-            const newTodo = document.createElement('li');
-            newTodo.className = 'list-group-item d-flex justify-content-between todos';
-            newTodo.appendChild(document.createTextNode(todos[i]));
-            newTodo.innerHTML += '<a href = "#" class ="delete-item"><i class="fa fa-remove"></i></a>';
-            newTodo.id = "todo" + todoList.childElementCount;
-            todoList.appendChild(newTodo);
-        }
-    }
-}
-
-let addTodo = function(){
+function addTodo(){
     // console.log("Gorev ekleniyor.");
-
-    let todos;
     const todoName = document.querySelector('#todoName').value;
+    const todoDay = document.querySelector('#todoDay').value;
     const todoList = document.querySelector('#todo-list');
 
-    const newTodo = document.createElement('li');
-    newTodo.className = 'list-group-item d-flex justify-content-between todos';
-    newTodo.appendChild(document.createTextNode(todoName));
-    newTodo.innerHTML += '<a href = "#" class ="delete-item"><i class="fa fa-remove"></i></a>';
+    const newTodo = document.createElement('div');
+    newTodo.className = 'col-lg-3 card-col';
+    newTodo.innerHTML += '<div class="card" style="width: 18rem;"><div class="card-img-top card-top">' + todoDay +
+    '<div class="card-body"><p class="card-text card-main">'+ todoName +'</p></div></div>';
     newTodo.id = "todo" + todoList.childElementCount;
     todoList.appendChild(newTodo);
 
-    if(localStorage.getItem("todos") == null){
+    addTodoToLocal(todoName, todoDay);
+    
+}
+
+function addTodoToLocal(todoName, todoDay){
+    let todos;
+
+    if (localStorage.getItem("todos") == null) {
         todos = [];
+        
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
+       
     }
 
-    todos.push(newTodo.textContent);
+    var newObj = {
+        todo_name : todoName,
+        todo_day : todoDay
+    }
+
+    todos.push(newObj);
 
     localStorage.setItem("todos", JSON.stringify(todos));
 }
