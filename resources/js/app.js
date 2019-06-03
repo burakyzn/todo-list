e6cbcb23b19607ff7a19d15c01cad60326104718
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-06-03 23:39:34 +0300

    months section edited. general code layout refreshed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,24 @@
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-const years = ['2019','2020','2021','2022','2023','2024','2025'];
+const years = [];
 const todoMonthSelect = document.querySelector('#todoMonth');
 const todoYearSelect = document.querySelector('#todoYear');
 const todoDaySelect = document.querySelector('#todoDay');
+const todoName = document.querySelector('#todoName');
 const todoList = document.querySelector('#todo-list');
 
-var this_month = new Date().getMonth();
-for(var i = this_month; i<months.length; i++){
-    var newTempMonth = document.createElement('option');
-    newTempMonth.value = i;
-    newTempMonth.innerHTML = months[i];
-    todoMonthSelect.appendChild(newTempMonth);
-}
+setup();
 
-for(var i = 0; i<years.length; i++){
-    var newTempYear = document.createElement('option');
-    newTempYear.value = years[i];
-    newTempYear.innerHTML = years[i];
-    todoYearSelect.appendChild(newTempYear);
-}
+function addTodo(){
+    let day = calculateTime(todoDaySelect.value, todoMonthSelect.value, todoYearSelect.value);
 
-for(var i = 0; i<31; i++){
-    var newTempDay = document.createElement('option');
-    newTempDay.value = i+1;
-    newTempDay.innerHTML = i+1;
-    todoDaySelect.appendChild(newTempDay);
+    let newTodo = document.createElement('div');
+    newTodo.className = 'col-lg-3 card-col';
+    newTodo.innerHTML += '<div class="card" style="width: 18rem;"><div class="card-img-top card-top">' + day + ' Day' +
+    '<div class="card-body"><p class="card-text card-main">'+ todoName.value +'</p><input type="button" id="btnDelete" class="btn btn-primary" value="Delete"></div></div>';
+    newTodo.id = "todo" + todoList.childElementCount;
+    todoList.appendChild(newTodo);
+
+    addTodoToLocal(todoName.value, todoDay.value, todoMonthSelect.value, todoYearSelect.value);
 }
 
 function getTodos() {
@@ -34,9 +28,9 @@ function getTodos() {
     if(todos != null){
         for (let i = 0; i < todos.length;i++){
 
-            var day = calculateTime(todos[i].todo_day,todos[i].todo_month,todos[i].todo_year);
+            let day = calculateTime(todos[i].todo_day,todos[i].todo_month,todos[i].todo_year);
 
-            const newTodo = document.createElement('div');
+            let newTodo = document.createElement('div');
             newTodo.className = 'col-lg-3 card-col';
             newTodo.innerHTML += '<div class="card" style="width: 18rem;"><div class="card-img-top card-top">' + day + ' Day' +
             '<div class="card-body"><p class="card-text card-main">'+ todos[i].todo_name +'</p><input type="button" id="btnDelete" class="btn btn-primary" value="Delete"></div></div>';
@@ -54,52 +48,27 @@ function addTodoToLocal(todoName, todoDay, todoMonth, todoYear){
         
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
-       
     }
 
-    var newTodoObj = {
+    todos.push({
         todo_name : todoName,
         todo_day : todoDay,
         todo_month : todoMonth,
-        todo_year : todoYear 
-    }
-
-    todos.push(newTodoObj);
+        todo_year : todoYear
+    });
 
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function calculateTime(day,month,year){
-    var date_today = new Date();
-    var date_todo = new Date(year, month, day, 0, 0, 0, 0);
+    let date_today = new Date();
+    let date_todo = new Date(year, month, day, 0, 0, 0, 0);
 
-    var second = Math.floor((date_todo- date_today) / 1000);
-    var minute = Math.floor(second / 60);
-    var hour = Math.floor(minute / 60);
-    var day = Math.floor(hour / 24);
+    let second = Math.floor((date_todo - date_today) / 1000);
+    let minute = Math.floor(second / 60);
+    let hour = Math.floor(minute / 60); 
 
-    return day;
-}
-
-function addTodo(){
-    const todoName = document.querySelector('#todoName').value;
-    const todoDay = document.querySelector('#todoDay').value;
-    const todoMonth = document.querySelector('#todoMonth').value;
-    const todoYear = document.querySelector('#todoYear').value;
-
-    console.log('todo day' + todoDay);
-    console.log('todo month' + todoMonth);
-    console.log('todo year' + todoYear);
-    var day = calculateTime(todoDay, todoMonth, todoYear);
-
-    const newTodo = document.createElement('div');
-    newTodo.className = 'col-lg-3 card-col';
-    newTodo.innerHTML += '<div class="card" style="width: 18rem;"><div class="card-img-top card-top">' + day + ' Day' +
-    '<div class="card-body"><p class="card-text card-main">'+ todoName +'</p><input type="button" id="btnDelete" class="btn btn-primary" value="Delete"></div></div>';
-    newTodo.id = "todo" + todoList.childElementCount;
-    todoList.appendChild(newTodo);
-
-    addTodoToLocal(todoName, todoDay, todoMonth, todoYear);
+    return Math.floor(hour / 24);
 }
 
 todoList.addEventListener("click", function(event){
@@ -111,7 +80,7 @@ todoList.addEventListener("click", function(event){
             let todos = JSON.parse(localStorage.getItem("todos"));
             localStorage.clear();
 
-            for (var i = 0 ; i < todos.length; i++){
+            for (let i = 0 ; i < todos.length; i++){
                 if(todos[i].todo_name == event.target.parentElement.firstChild.textContent){
                     todos.splice(i,1);
                     break;
@@ -125,3 +94,57 @@ todoList.addEventListener("click", function(event){
         }
     }
 })
+
+todoYearSelect.addEventListener('change',function(){
+    todoMonthSelect.innerHTML = "";
+    let this_year = new Date().getFullYear();
+
+    if(this.value > this_year){
+        for(let i = 0; i<months.length; i++){
+            let newTempMonth = document.createElement('option');
+            newTempMonth.value = i;
+            newTempMonth.innerHTML = months[i];
+            todoMonthSelect.appendChild(newTempMonth);
+        }
+    } else {
+        let this_month = new Date().getMonth();
+        for(let i = this_month; i<months.length; i++){
+        let newTempMonth = document.createElement('option');
+        newTempMonth.value = i;
+        newTempMonth.innerHTML = months[i];
+        todoMonthSelect.appendChild(newTempMonth);
+        }
+    }
+})
+
+function setup(){
+    let this_year = new Date().getFullYear();
+
+    for(let i = this_year; i<(this_year+5);i++){
+        years.push(i);
+    }
+
+    let this_month = new Date().getMonth();
+    for(let i = this_month; i<months.length; i++){
+        let newTempMonth = document.createElement('option');
+        newTempMonth.value = i;
+        newTempMonth.innerHTML = months[i];
+        todoMonthSelect.appendChild(newTempMonth);
+    }
+    
+    for(let i = 0; i<years.length; i++){
+        let newTempYear = document.createElement('option');
+        newTempYear.value = years[i];
+        newTempYear.innerHTML = years[i];
+        todoYearSelect.appendChild(newTempYear);
+    }
+    
+    for(let i = 0; i<31; i++){
+        let newTempDay = document.createElement('option');
+        newTempDay.value = i+1;
+        newTempDay.innerHTML = i+1;
+        todoDaySelect.appendChild(newTempDay);
+    }
+
+    getTodos();
+}
